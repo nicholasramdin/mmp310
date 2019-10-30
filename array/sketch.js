94a0440f7752dd0a9007c2133eb6635a08d8284c
nicholasramdin <31458944+nicholasramdin@users.noreply.github.com>
2019-10-30 15:30:33 -0400

    Added Array
    need to animate array next

---
+++ array/sketch.js
@@ -0,0 +1,32 @@
+var sizes = [100, 80, 120, 90, 100, 110];
+var position = [0, 10, -10, 20, -20, 0];
+
+
+function setup() {
+	createCanvas(640, 360);
+}
+function draw() {
+	background(51);
+	noStroke();
+	
+	for (let i = 0; i < sizes.length; i++) {
+		spaceCadet(50 + i * 100, 100, sizes[i]);
+        
+        position[i] += sizes[i];
+        
+        
+	}
+}
+
+function spaceCadet(x,y,s){
+
+    
+fill('white');
+	ellipse(x, y, s, s - 25);
+    
+    stroke('black');
+    fill('blue');
+    rect(x - 10, y - 20, s/4, s/4); // left visor box
+    rect(x + 10 , y - 20, s/4, s/4); // right visor box
+    	
+}
